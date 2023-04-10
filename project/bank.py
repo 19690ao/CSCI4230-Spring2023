1682a110e40c8e4513b247c9530cb997e8656f5f
19690ao <ortaa@rpi.edu>
2023-04-10 13:48:43 -0400

    Setup Crypt/Server

--- project/bank.py
+++ project/bank.py
@@ -9,6 +9,10 @@ from PrivateKey import aes
 
 class Bank:
     def __init__(self):
+        self.setupCrypt()
+        self.setupServer()
+
+    def setupCrypt(self):
         #get all the users and their associated hashes and their money
         self.usertopass = json.loads(open("atm_secret/usertohashpass.txt", "r").read()) 
         self.usertomoney = json.loads(open("atm_secret/usertomoney.txt", "r").read())
@@ -19,6 +23,9 @@ class Bank:
         self.counter = 0
         #random large prime
         self.p= 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
+        
+
+    def setupServer(self):
         self.s = socket.socket()
         self.s.bind(('127.0.0.1',9999))
         self.s.listen(5)
